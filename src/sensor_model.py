748698838808ca0729076f9e97598a176ba1f855
alexcuel <alexcuel@mit.edu>
2019-03-26 15:30:22 -0400

    increase sensor range

--- src/sensor_model.py
+++ src/sensor_model.py
@@ -20,7 +20,7 @@ class SensorModel:
         #meters/measurement in lookup table 
         self.grain = rospy.get_param("~lookup_grain")
         #Get lookup table: Values listed as (a_hit, a_short, a_max, a_rand, sigma, max_range, dz)
-        self.prob_lookup = sensor_lookup.SensorTable(.74, .07, .07, .12, .5, 10, self.grain)
+        self.prob_lookup = sensor_lookup.SensorTable(.74, .07, .07, .12, .5, 30, self.grain)
         ####################################
         # TODO
         # Precompute the sensor model here
@@ -78,7 +78,7 @@ class SensorModel:
         # This produces a matrix of size N x num_beams_per_particle 
         scans = self.scan_sim.scan(particles)
         #Bound the distance of a scan to 9.9
-        scans = np.clip(scans, 0, 9.9)
+        scans = np.clip(scans, 0, 29.9)
         #Return probabilities
         return self.scans_to_probs(scans, observations, self.grain)
         ####################################
@@ -117,7 +117,7 @@ class SensorModel:
         take in scans, observations, and grain
         '''
         observations = np.array(laser_scan.ranges)[::self.take_nth_beam]
-        observations = np.clip(observations, 0, 9.9)
+        observations = np.clip(observations, 0, 29.9)
         if observations.shape[0] != scans.shape[1]:
             print("WARNING: observations and ray cast sizes different")
         #Initialize a matrix of probabilities associated with each ray
